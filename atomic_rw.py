a59b75a1e3346022a0b5fdff87ad34e3bce9a4bc
Yuichiro Mori <ymorired@gmail.com>
2015-02-03 01:12:21 -0800

    finally handle unicode properly

--- atomic_rw.py
+++ atomic_rw.py
@@ -12,19 +12,40 @@ class AtomicRWer(object):
 
         self.file_path = os.path.abspath(file_path)
         self.fd = None
+        self.r_fd = None
 
     def open_for_write(self):
-
         self.fd = open(self.file_path, 'ab')
 
     def write(self, record):
         if self.fd is None:
             raise Exception('File descriptor is not open!')
 
-        checksum = '{:8x}'.format(zlib.crc32(record)).encode()
+        checksum = '{:8x}'.format(zlib.crc32(record)).encode('utf-8')
         self.fd.write(record + b' ' + checksum + b'\n')
 
     def close_for_write(self):
         self.fd.close()
         self.fd = None
+
+    def open_for_read(self):
+        self.r_fd = open(self.file_path, 'rb')
+
+    def read(self):
+        if self.r_fd is None:
+            raise Exception('File descriptor is not open!')
+
+        ret_lines = []
+        for line in self.r_fd:
+            record, checksum = line.strip().rsplit(b' ', 1)
+            if checksum.decode() == '{:8x}'.format(zlib.crc32(record)):
+                ret_lines.append(record.decode('utf8'))
+            else:
+                print('checksum error for record {}'.format(record))
+
+        return ret_lines
+
+    def close_for_read(self):
+        self.r_fd.close()
+        self.r_fd = None
 
--- test/atomic_rw_test.py
+++ test/atomic_rw_test.py
@@ -23,9 +23,21 @@ class FileListerTest(unittest.TestCase):
         rwer.write(json.dumps({
             'test': 'testtttt!',
             'num': 1,
-        }))
+            # 'bin': '日本語',
+            'unic': u'ユニコード',
+        }, ensure_ascii=False).encode('utf8'))
 
         rwer.close_for_write()
+
+        rwer.open_for_read()
+
+        for line in rwer.read():
+            import pprint
+            pprint.pprint(json.loads(line))
+            # print line
+
+        rwer.close_for_read()
+
 
 if __name__ == '__main__':
     unittest.main()
